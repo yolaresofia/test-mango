55c39af1e3aad2388b2aa23f6de434598ca4f0a8
yolaresofia <yolaresofia@gmail.com>
2024-10-10 02:34:58 +0200

    update styles and link hrefs on home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,11 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex justify-center items-center flex-col">
-      <h1 className="text-3xl">MANGO techical test</h1>
+    <div className="flex justify-center items-center flex-col h-screen">
+      <h1 className="text-3xl pb-6">MANGO techical test</h1>
       <div className="flex space-x-4">
-        <Link href="/exercise2"><button className="border-2 border-black rounded-sm px-4 py-2">go to exercise 1</button></Link>
-        <Link href="/exercise1"><button className="border-2 border-black rounded-sm px-4 py-2">go to exercise 2</button></Link>
+        <Link href="/exercise1"><button className="border-2 border-black rounded-sm px-4 py-2">go to exercise 1</button></Link>
+        <Link href="/exercise2"><button className="border-2 border-black rounded-sm px-4 py-2">go to exercise 2</button></Link>
       </div>
     </div>
   );
